19c5a3b282620f6d17c966d935bf1a2385505ac5
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-03 22:23:33 +0100

    edits

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,26 +18,18 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
                 ),
-                Expanded(
-                  child: ReusableCard()
-                ),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),)),
               ],
             ),
           ),
-          Expanded(
-            child: ReusableCard()
-          ),
+          Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),)),
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  child: ReusableCard()
-                ),
-                Expanded(
-                  child: ReusableCard()
-                ),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),)),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),)),
               ],
             ),
           ),
@@ -48,16 +40,17 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key? key,
-  }) : super(key: key);
+  
+  ReusableCard({required this.colour});
+
+  final Color colour;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
