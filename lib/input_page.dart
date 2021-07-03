6e9928210f697f374cef9b2555340b8421475ca9
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-03 22:09:50 +0100

    edits

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,82 +12,56 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
               children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 200.0,
-                    width: 170.0,
+                Expanded(child: Container(
                     margin: EdgeInsets.all(15.0),
                     decoration: BoxDecoration(
                       color: Color(0xFF1D1E33),
                       borderRadius: BorderRadius.circular(10.0),
                     ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 200.0,
-                    width: 170.0,
+                  ),),
+                Expanded(child: Container(
                     margin: EdgeInsets.all(15.0),
                     decoration: BoxDecoration(
                       color: Color(0xFF1D1E33),
                       borderRadius: BorderRadius.circular(10.0),
                     ),
-                  ),
-                ),
+                  ),),
               ],
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 200.0,
-                    width: 170.0,
+          ),
+          Expanded(child: Container(
                     margin: EdgeInsets.all(15.0),
                     decoration: BoxDecoration(
                       color: Color(0xFF1D1E33),
                       borderRadius: BorderRadius.circular(10.0),
                     ),
-                  ),
-                ),
+                  ),),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),),
+                Expanded(child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),),
               ],
             ),
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 200.0,
-                    width: 170.0,
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 200.0,
-                    width: 170.0,
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
