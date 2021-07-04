ac8a12456167ec52a314c8c2a28878127ca636e4
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-04 22:30:27 +0100

    edits

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_calculator/icon_content.dart';
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -25,11 +27,8 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     cardChild: IconContent(
-                      icon: Icon(
-                        FontAwesomeIcons.mars,
-                        size: 80.0,
-                      ),
-                      text: 'MALE',
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
                     ),
                     colour: Color(activeCardColour),
                   ),
@@ -37,11 +36,8 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                     child: ReusableCard(
                   cardChild: IconContent(
-                    icon: Icon(
-                      FontAwesomeIcons.venus,
-                      size: 80.0,
-                    ),
-                    text: 'FEMALE',
+                    icon: FontAwesomeIcons.venus,
+                    label: 'FEMALE',
                   ),
                   colour: Color(activeCardColour),
                 )),
@@ -76,52 +72,6 @@ class _InputPageState extends State<InputPage> {
             height: bottomContainerHeight,
           ),
         ],
-      ),
-    );
-  }
-}
-
-class IconContent extends StatelessWidget {
-  const IconContent({required this.icon, required this.text});
-
-  final Icon icon;
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        icon,
-        SizedBox(
-          height: 15,
-        ),
-        Text(
-          text,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8D98),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour, this.cardChild});
-
-  final Color colour;
-  final Widget? cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
