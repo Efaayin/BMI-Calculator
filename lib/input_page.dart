f12711b17d011b721ab33ff9c70669e898b9ca46
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-04 21:23:47 +0100

    edits

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -67,9 +67,10 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour});
+  ReusableCard({required this.colour, this.cardChild});
 
   final Color colour;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
