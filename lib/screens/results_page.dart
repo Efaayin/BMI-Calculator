2449558b0516d77af12f81a5ea3ae3b7707c2c6c
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-07 13:53:35 +0100

    edits

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -4,8 +4,6 @@ import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
 
 class ResultsPage extends StatelessWidget {
-  const ResultsPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
